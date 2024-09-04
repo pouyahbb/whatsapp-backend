0430973e0b69426dfc43b36bb63ff0d6087ea7ca
pouyaHobbi <pouyahobbi504@gmail.com>
2024-09-05 00:29:21 +0330

    Add all middlewares

--- src/app.js
+++ src/app.js
@@ -1,7 +1,50 @@
 import express from "express";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import helmet from "helmet";
+import mongoSanatize from "express-mongo-sanitize";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import fileUpload from "express-fileupload";
+import cors from "cors";
 
+// dotenv-config
+dotenv.config();
 // create express app
 const app = express();
+
+// morgan middleware
+if (process.env.NODE_ENV !== "production") {
+  app.use(morgan("dev"));
+}
+
+// Helmet middleware
+app.use(helmet());
+
+// parse json request body
+app.use(express.json());
+
+// parse json request url
+app.use(express.urlencoded({ extended: true }));
+
+// sanitize request data
+app.use(mongoSanatize());
+
+// enable cookie parser
+app.use(cookieParser());
+
+// gzip compression data that come from request body(make faster)
+app.use(compression());
+
+// file upload
+app.use(
+  fileUpload({
+    useTempFiles: true,
+  })
+);
+
+// cors
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("hello from server");
