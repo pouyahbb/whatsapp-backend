485956fd2b34efc22267f64e02becf9e8a3e7a05
pouyaHobbi <pouyahobbi504@gmail.com>
2024-09-10 00:51:15 +0330

    Finish chat and user functionalities

--- services/user.service.js
+++ services/user.service.js
@@ -3,6 +3,18 @@ import { UserModel } from "../models/index.js";
 
 export const findUser = async (userId) => {
   const user = await UserModel.findById(userId);
-  if (!user) throw createHttpError.BadRequest("Please fill all fields");
+  if (!user) throw createHttpError.BadRequest("Please fill all fields.");
   return user;
+};
+
+export const searchUsers = async (keyword, userId) => {
+  const users = await UserModel.find({
+    $or: [
+      { name: { $regex: keyword, $options: "i" } },
+      { email: { $regex: keyword, $options: "i" } },
+    ],
+  }).find({
+    _id: { $ne: userId },
+  });
+  return users;
 };
